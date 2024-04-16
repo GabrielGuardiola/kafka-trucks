69e0b61a98f0e40df2db568e6dfbd4fac9ff77d4
Gabodev <51827577+Gguardiola@users.noreply.github.com>
2024-04-16 14:04:24 +0200

    topic

--- src/main/java/com/helloworld/kafka/producers/AsyncProducer.java
+++ src/main/java/com/helloworld/kafka/producers/AsyncProducer.java
@@ -11,6 +11,30 @@ import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.clients.producer.Callback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+// Camiones
+ 
+// 5 camiones
+ 
+// Cantidad de kilómetros que ha realizado cada camión.
+ 
+// Simular los camiones en un único productor.
+ 
+// Los camiones van a mandar como mensaje el kilométro de la carretera por la que van
+ 
+// Los camiones van a mandar 10 mediciones. en cada medición avanzan 1,3 km.
+ 
+// Siempre van por la misma carretera
+ 
+// Pero no sabemos en qué kilómetro de la carreta empiezan
+ 
+// En la simulación cada camión debe empezar en un kilómetro aleatorio entre 10 y 20.
+ 
+// Objetivos:
+// - Plantear la estructura de topics ---> Un topic. km_posicion (gps), en la clave el id de camión.
+// - Implementar el productor  --> productor asíncrono, opcionalmente tratar errores con callback
+// - Utilizar un consumidor kafka-console-consumer
+// - Objetivo parcial: 
+//     En el consumidor debe aparecer todos los datos de todos los camiones.
 
 public class AsyncProducer {
 
@@ -23,22 +47,20 @@ public class AsyncProducer {
         props.put("key.serializer", StringSerializer.class);
         props.put("value.serializer", StringSerializer.class);
         
-        final String topic = "test-topic";
+        final String topic = "truck-topic-monitoring";
 
-        String[] users = {"eabara", "jsmith", "sgarcia", "jbernard", "htanaka", "awalther"};
-        String[] items = {"book", "alarm clock", "t-shirts", "gift card", "batteries"};
-        final Producer<String, String> producer = new KafkaProducer<>(props);
-            
-        final Random rnd = new Random();
-        final Long numMessages = 10L;
-        for (Long i = 0L; i < numMessages; i++) {
-            String user = users[rnd.nextInt(users.length)];
-            String item = items[rnd.nextInt(items.length)];
-            ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topic, user, item);
-            producer.send(producerRecord,new ProducerCallback());
+        Producer<String, String> producer = new KafkaProducer<>(props);
+
+        for (int i = 0; i < 5; i++) {
+            Random random = new Random();
+            int initialKm = random.nextInt(10) + 10;
+            log.info("Camión {} empieza en el kilómetro {}", i, initialKm);
+            for (int j = 0; j < 10; j++) {
+                initialKm += 1.3;
+                ProducerRecord<String, String> record = new ProducerRecord<>(topic, Integer.toString(i), Integer.toString(initialKm));
+                producer.send(record, new ProducerCallback());
+            }
         }
-        System.out.printf("%s events were produced to topic %s%n", numMessages, topic);
-        
         producer.close();
 
     }
